0965a80fbd841723a92f956170063986c772e897
marija-stojanova <marijaslash@yahoo.com>
2022-07-18 13:54:10 +0200

    something not alright with correlations

--- add_columns.py
+++ add_columns.py
@@ -3,20 +3,19 @@ import os
 
 complete = pd.DataFrame()
 
-directory = "1H/"
+directories = ["1H/", '1H_obstacle/', '2nd/', '2nd_obstacle/', '3rd/', '3rd_obstacle/', 'CA/', 'CA_doubles/', 'CA_move/', 'CA_obstacle/']
+LoS = [1,0,1,0,1,0,1,1,1,0]
 
-files = os.listdir(directory)
-LoS = 1
-
-for i in files:
-    antennaPosition = i.split('_')[0]
-    distance = i.split('_')[1]
-    df = pd.read_csv(directory + i, delimiter='\t')
-    df['distance'] = distance
-    df['antenna'] = antennaPosition
-    df['LoS'] = LoS
-    df.to_csv(directory + i, sep='\t', index=False)
-    complete = pd.concat([complete, df])
-
-complete.to_csv(directory + "complete_data.csv", sep='\t', index=False)
-
+for d in range(len(directories)):
+	files = os.listdir(directories[d])	
+	for i in files:
+		antennaPosition = i.split('_')[0]
+		distance = i.split('_')[1]
+		df = pd.read_csv(directories[d] + i, delimiter='\t')
+		df['distance'] = distance
+		df['antenna'] = antennaPosition
+		df['LoS'] = LoS[d]
+		df.to_csv(directories[d] + i, sep='\t', index=False)
+		complete = pd.concat([complete, df])
+	complete.to_csv(directories[d] + "complete_data.csv", sep='\t', index=False)
+	complete = pd.DataFrame()
--- clean_data.sh
+++ clean_data.sh
@@ -1,4 +1,5 @@
 rename "s/.pcap.data/csv/" */*.data
+rm */*complete_data*
 sed -i 's/: //g' */*.csv
 # all all the column names 
 sed -i.csv 1i "time mcs nss bw rssi throughput_monitor datarate pktlost_ack nbpkt_monitor retry nbpkt_ack pktlost_seq_initial pktlost_seq_new bad_fcs" */*.csv
@@ -6,3 +7,11 @@ sed -i.csv 1i "time mcs nss bw rssi throughput_monitor datarate pktlost_ack nbpk
 # sed -i 2,6d */*.csv
 sed -i 's/ /\t/g' */*.csv
 rm */*.csv.csv
+sed -i 's/\t\t\t\t\t\t/\tNaN\tNan\tNaN\tNan\tNaN\tNaN/g' */*.csv
+
+kako e sekoas prashanje dali vrede neshto da se naprave
+bekapot na podatocite
+da gi vide ona skriptite 
+iminjata na podatocite ne gi smeni
+ampdu brojot kolku pati mu kaza
+od koga se bunish za presmetkite so ACK
